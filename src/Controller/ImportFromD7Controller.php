0d27501431e82686ac3f1bee56bb531ae10b40a0
kouwa stephane <kksasteph888@gmail.com>
2024-11-30 12:18:48 +0100

    Maj

--- src/Controller/ImportFromD7Controller.php
+++ src/Controller/ImportFromD7Controller.php
@@ -1,6 +1,5 @@
 <?php
-
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace Drupal\migration_hbk_auto\Controller;
 
@@ -10,18 +9,25 @@ use Drupal\Core\Controller\ControllerBase;
  * Returns responses for Migration Hbk Auto routes.
  */
 final class ImportFromD7Controller extends ControllerBase {
-
+  
   /**
    * Builds the response.
    */
   public function __invoke(): array {
-
     $build['content'] = [
-      '#type' => 'item',
-      '#markup' => $this->t('It works!'),
+      '#type' => 'html_tag',
+      '#tag' => 'div',
+      '#value' => 'Chargement ...',
+      '#attributes' => [
+        'id' => 'migrate-app-build'
+      ],
+      '#attached' => [
+        'library' => [
+          'migration_hbk_auto/buildinterface'
+        ]
+      ]
     ];
-
+    
     return $build;
   }
-
 }
