29cc3f1d23c49658f709f1adae326cb506eacff5
kouwa stephane <kksasteph888@gmail.com>
2024-12-02 12:04:49 +0100

    Maj

--- hbk_atomique_vuejs/src/components/BundlesDisplay.vue
+++ hbk_atomique_vuejs/src/components/BundlesDisplay.vue
@@ -2,17 +2,17 @@
   <!--
 Ce fichier permet d'affichager toutes les configurations.
 -->
-  <div class="card" :style="{ 'max-width': '1200px', padding: '1rem' }">
+  <div class="card" :style="{ 'max-width': '1200px', padding: '0.5rem' }">
+    <Button :label="numbersBundles" @click="buildBundle" />
     <Accordion value="0">
-      <AccordionPanel v-for="tab in tabsEntitiesDefinitions.items" :key="tab.id" :value="tab.id">
+      <AccordionPanel v-for="tab in bundles.items" :key="tab.id" :value="tab.id">
         <AccordionHeader>
           <div>
             {{ tab.title }} <i>({{ tab.id }})</i>
           </div>
         </AccordionHeader>
         <AccordionContent>
-          <BundlesDisplay :bundles="tab.bundles"></BundlesDisplay>
-          <Button label="Afficher le code json" @click="openClose(tab)" />
+          <Button label="Verifier la configuration" @click="CheckConfig(tab)" />
           <Dialog v-model:visible="tab.show_json" maximizable modal :header="tab.title" :style="{ width: '50rem' }" :breakpoints="{ '1199px': '75vw', '575px': '90vw' }">
             <pre :style="{ 'font-size': '10px' }">{{ tab.content }}</pre>
           </Dialog>
@@ -20,11 +20,10 @@ Ce fichier permet d'affichager toutes les configurations.
       </AccordionPanel>
     </Accordion>
   </div>
-  <Button severity="secondary" @click="loadAllEntities"> Load all entities </Button>
 </template>
 
 <script setup>
-import { reactive } from "vue";
+import { reactive, defineProps, computed } from "vue";
 import config from "../rootConfig";
 import Accordion from "primevue/accordion";
 import AccordionPanel from "primevue/accordionpanel";
@@ -32,30 +31,40 @@ import AccordionHeader from "primevue/accordionheader";
 import AccordionContent from "primevue/accordioncontent";
 import Button from "primevue/button";
 import Dialog from "primevue/dialog";
-import BundlesDisplay from "./BundlesDisplay.vue";
+
+const props = defineProps(["bundles"]);
 
 /**
  * Contient les definitions des
  */
-const tabsEntitiesDefinitions = reactive({ items: [] });
+const bundles = reactive({ items: [] });
 
-/**
- * Charge les definitions d'entites.
- */
-function loadAllEntities() {
-  config.get("/migrateexport/entities-definitions").then((result) => {
-    if (result.data) {
-      for (var i in result.data) {
-        const definition = result.data[i];
-        tabsEntitiesDefinitions.items.push({ title: definition.label, content: definition, id: i, show_json: false, bundles: definition.bundles });
-      }
+const numbersBundles = computed(() => {
+  if (props.bundles) {
+    return "List bundles (" + Object.keys(props.bundles).length + ")";
+  }
+  return "Aucun bundle";
+});
+
+const buildBundle = () => {
+  if (props.bundles) {
+    bundles.items = [];
+    for (var j in props.bundles) {
+      const bundle = props.bundles[j];
+      bundles.items.push({ title: bundle.label, content: bundle, id: j, show_json: false });
     }
+  }
+};
+/**
+ * Permet de verifier la configuration.
+ *
+ */
+const CheckConfig = (tab) => {
+  config.get("/migrateexport/migrate-export-entities/" + tab.id).then((result) => {
+    console.log("result bundles : ", result);
   });
-}
-
-function openClose(tab) {
-  tab.show_json = tab.show_json ? false : true;
-}
+  //
+};
 
 // https://vuejs.org/api/#composition-api
 </script>
